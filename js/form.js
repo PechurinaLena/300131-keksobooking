2ac8e48727196e1057b13ebec29810ca0c8a0354
Maria <tirimaria@gmail.com>
2017-01-29 20:50:40 +0300

    module3-task1 'this' version

--- js/form.js
+++ js/form.js
@@ -1,32 +1,23 @@
-// Показ карточки объявления
-
-// При нажатии на любой из элементов .pin ему должен добавляться класс pin--active и должен показываться элемент .dialog. 
-// Если до этого у другого элемента существовал класс pin--active, то у этого элемента класс нужно убрать
-
-//Показ карточки объявления
+'use strict';
 var pins = document.querySelectorAll('.pin');
 var dialog = document.querySelector('.dialog');
 var dialogClose = document.querySelector('.dialog__close');
 
 function toggle() {
-  if (this.classList.contains('pin--active')) {
-    this.classList.remove('pin--active');
-        dialog.style.display='none';
-  }    else {
-        this.classList.add('pin--active');
-        dialog.style.display="block";
+  for (var i = 0; i < pins.length; i++) {
+    pins[i].classList.remove('pin--active');
+            // dialog.style.display='none';
   }
+  this.classList.add('pin--active');
 }
 
 
 dialogClose.addEventListener('click', function () {
   dialog.style.display="none";
-  if (this.classList.contains('pin--active')) {
-    this.classList.remove('pin--active')
-  }
+//  здесь еще не доделано
 });
 
 for (var i = 0; i < pins.length; i++) {
-    var element = pins[i];
-    element.addEventListener('click', toggle);
+  var element = pins[i];
+  element.addEventListener('click', toggle);
 }
