3a25b17dc5d6fe44dd77e445b40f3803aada09ae
Maria <tirimaria@gmail.com>
2017-01-30 23:02:28 +0300

    task 4 completed, 'this' replaced with 'event target'

--- js/form.js
+++ js/form.js
@@ -4,11 +4,11 @@ var dialog = document.querySelector('.dialog');
 var dialogClose = document.querySelector('.dialog__close');
 var i;
 
-function toggle() {
+function toggle(evt) {
   for (i = 0; i < pins.length; i++) {
     pins[i].classList.remove('pin--active');
   }
-  this.classList.add('pin--active');
+  evt.target.classList.add('pin--active');
   dialog.style.display = 'block';
 }
 
@@ -34,12 +34,12 @@ inputPrice.min = 1000;
 inputPrice.max = 1000000;
 inputAddress.required = true;
 inputPrice.placeholder = 1000;
-
-
 var myForm = document.forms.my;
-var checkinTime = myForm.elements.time; //массив options с временем заезда
-var checkoutTime = myForm.elements.timeout; //массив options с временем выезда
+var checkinTime = myForm.elements.time; // элемент select со временами въезда
+var checkoutTime = myForm.elements.timeout; // элемент select со временами выезда
 var housing = myForm.elements.type;
+var roomNumber = myForm.elements.room_number;
+var guestsNumber = myForm.elements.capacity;
 
 function checkinTimeChanged() {
   checkoutTime.value = checkinTime.value;
@@ -49,28 +49,38 @@ function checkoutTimeChanged() {
 }
 
 function housingTypeChanged() {
-  for (i = 0; i < housing.options.length; i++) {
-    var option = housing.options[i];
-    if (option.selected) {
-      switch (option.value) {
-        case 'apartment':
-          inputPrice.min = 1000;
-          inputPrice.placeholder = 1000;
-          break;
-        case 'hovel':
-          inputPrice.min = 0;
-          inputPrice.placeholder = 0;
-          break;
-        case 'palace':
-          inputPrice.min = 10000;
-          inputPrice.placeholder = 10000;
-          break;
-      }
-    }
+  switch (housing.value) {
+    case 'apartment':
+      inputPrice.min = 1000;
+      inputPrice.placeholder = 1000;
+      break;
+    case 'hovel':
+      inputPrice.min = 0;
+      inputPrice.placeholder = 0;
+      break;
+    case 'palace':
+      inputPrice.min = 10000;
+      inputPrice.placeholder = 10000;
+      break;
   }
+}
 
+
+function roomNumberChanged() {
+  switch (roomNumber.value) {
+    case '1':
+      guestsNumber.value = 'noguests';
+      break;
+    case '2':
+      guestsNumber.value = '3guests';
+      break;
+    case '100':
+      guestsNumber.value = '3guests';
+      break;
+  }
 }
 
 checkinTime.addEventListener('change', checkinTimeChanged);
 checkoutTime.addEventListener('change', checkoutTimeChanged);
 housing.addEventListener('change', housingTypeChanged);
+roomNumber.addEventListener('change', roomNumberChanged);
