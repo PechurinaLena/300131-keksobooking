72c55e60c9602dee756f9fa9bbb084db288403d4
Maria <tirimaria@gmail.com>
2017-02-04 12:59:56 +0300

    for review

--- js/form.js
+++ js/form.js
@@ -1,22 +1,53 @@
 'use strict';
-var pins = document.querySelectorAll('.pin');
+
+var tokyoPins = document.querySelector('.tokyo__pin-map');
 var dialog = document.querySelector('.dialog');
 var dialogClose = document.querySelector('.dialog__close');
 var activePin;
+var eventCodeEnter = 13;
+var eventCodeEscape = 27;
 
-Array.prototype.forEach.call(pins, function (pin) {
-  pin.addEventListener('click', function () {
-    if (activePin) {
-      activePin.classList.remove('pin--active');
-    }
-    pin.classList.add('pin--active');
-    dialog.style.display = 'block';
-    activePin = pin;
-  });
-});
+var pinsClickHandler = function (evt) {
+  if (evt.type === 'keydown' && evt.keyCode !== eventCodeEnter) {
+    return;
+  }
+  var target = evt.target;
+
+  // if (evt.type === 'keydown') {
+  //   if (evt.keyCode === eventCodeEnter) {
+  //     dialog.style.display = 'block';
+  //     dialogClose.focus();
+  //   } else {
+  //     return;
+  //   }
+  // }
+
+  if (target.tagName === 'IMG') {
+    target = target.parentNode;
+  }
+
+  if (activePin) {
+    activePin.classList.remove('pin--active');
+  }
+  target.classList.add('pin--active');
+  dialog.style.display = 'block';
+  dialogClose.focus();
+  activePin = target;
+
+};
+
+tokyoPins.addEventListener('click', pinsClickHandler, true);
+tokyoPins.addEventListener('keydown', pinsClickHandler, true);
 
 dialogClose.addEventListener('click', function () {
   dialog.style.display = 'none';
+});
+
+dialogClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === eventCodeEscape || evt.keyCode === eventCodeEnter) {
+    dialog.style.display = 'none';
+    dialogClose.setAttribute('aria-pressed', 'true');
+  }
 });
 
 
