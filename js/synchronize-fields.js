8b37350db39f6dc927a5b9bf7d25e994ada68e0d
Maria <tirimaria@gmail.com>
2017-02-18 14:28:12 +0300

    merge

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -1,11 +1,14 @@
 'use strict';
 
-window.synchronizeFields = function (inputField, outputField, inputArray, outputArray, property) {
-  var inputFieldsSync = function (evt) {
-    var inputValue = inputField.value;
-    var index = inputArray.indexOf(inputValue);
-    var outputValue = outputArray[index];
-    outputField[property] = outputValue;
+window.synchronizeFields = (function () {
+  return function (inputField, outputField, inputArray, outputArray, property) {
+    var inputFieldsSync = function (evt) {
+      var inputValue = inputField.value;
+      var index = inputArray.indexOf(inputValue);
+      var outputValue = outputArray[index];
+      outputField[property] = outputValue;
+    };
+    inputField.addEventListener('change', inputFieldsSync);
   };
-  inputField.addEventListener('change', inputFieldsSync);
-};
+})();
+
